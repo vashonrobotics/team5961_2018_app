e08211e417d6febe4614011633bfdfddb4851c9a
Caz-person <cullimore.cazmire@vashonsd.org>
2017-09-30 13:14:22 -0700

    DriveTrain added a class that controls mecanum and non-mecanum movement (mecanum might not work correctly)
    Autonomous
    
     added register line
     got it ready for using DriveTrain
    
     Teleop
     got it ready for using DriveTrain

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonomousMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonomousMode.java
@@ -1,33 +1,29 @@
 package org.firstinspires.ftc.teamcode;
 
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
 
+import java.util.ArrayList;
+
 /**
  * Created by FTC on 9/23/2017.
+ * updated by Caz
  */
 
+@Autonomous(name = "Vashon 5961 Autonomous", group = "Vashon 5961")
 public class AutonomousMode extends LinearOpMode {
-    private DcMotor frontLeft;
-    private DcMotor frontRight;
-    private DcMotor backLeft;
-    private DcMotor backRight;
+    private ArrayList baseMotorArray;
     @Override
     public void runOpMode() throws InterruptedException {
-        frontLeft = hardwareMap.dcMotor.get("front Left");
-        frontRight = hardwareMap.dcMotor.get("front Right");
-        backLeft = hardwareMap.dcMotor.get("back Left");
-        backRight = hardwareMap.dcMotor.get("back Right");
-
-
-
-    }
-    public void driveTrain(Double power, Integer Time) {
-        frontLeft.setPower(power);
-        frontRight.setPower(power);
-        backLeft.setPower(power);
-        backRight.setPower(power);
-
+        // get wheels from config
+        baseMotorArray.add(hardwareMap.dcMotor.get("front Left"));
+        baseMotorArray.add(hardwareMap.dcMotor.get("front Right"));
+        baseMotorArray.add(hardwareMap.dcMotor.get("back Left"));
+        baseMotorArray.add(hardwareMap.dcMotor.get("back Right"));
+        ((DcMotor)baseMotorArray.get(1)).setDirection(DcMotor.Direction.REVERSE);
+        ((DcMotor)baseMotorArray.get(3)).setDirection(DcMotor.Direction.REVERSE);
 
     }
+
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOpMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOpMode.java
@@ -6,6 +6,8 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
+import java.util.ArrayList;
+
 /**
  * Created by FTC on 9/23/2017.
  * updated by Caz
@@ -18,21 +20,26 @@ public class TeleOpMode extends OpMode{
     private DcMotor frontRight;
     private DcMotor backLeft;
     private DcMotor backRight;
-
-
+    private ArrayList baseMotorArray;
     @Override
     public void init() {
-        frontLeft = hardwareMap.dcMotor.get("front Left");
-        frontRight = hardwareMap.dcMotor.get("front Right");
-        backLeft = hardwareMap.dcMotor.get("back Left");
-        backRight = hardwareMap.dcMotor.get("back Right");
+        baseMotorArray.add(hardwareMap.dcMotor.get("front Left"));
+        baseMotorArray.add(hardwareMap.dcMotor.get("front Right"));
+        baseMotorArray.add(hardwareMap.dcMotor.get("back Left"));
+        baseMotorArray.add(hardwareMap.dcMotor.get("back Right"));
+        ((DcMotor)baseMotorArray.get(1)).setDirection(DcMotor.Direction.REVERSE);
+        ((DcMotor)baseMotorArray.get(3)).setDirection(DcMotor.Direction.REVERSE);
     }
 
     @Override
     public void loop() {
-        frontLeft.setPower(gamepad1.right_stick_y-gamepad1.right_stick_x);
-        frontRight.setPower(gamepad1.right_stick_y+gamepad1.right_stick_x);
-        backLeft.setPower(gamepad1.right_stick_y-gamepad1.right_stick_x);
-        backRight.setPower(gamepad1.right_stick_y+gamepad1.right_stick_x);
+//        I'm keeping this because I don't know if we are using mecanum wheels for sure
+//        ArrayList powerArray = new ArrayList();
+//        powerArray.add(gamepad1.right_stick_y-gamepad1.right_stick_x);
+//        powerArray.add(gamepad1.right_stick_y+gamepad1.right_stick_x);
+//        powerArray.add(gamepad1.right_stick_y-gamepad1.right_stick_x);
+//        powerArray.add(gamepad1.right_stick_y+gamepad1.right_stick_x);
+//        DriveTrain.nonMecanum(baseMotorArray, powerArray);
+        DriveTrain.mecanum(baseMotorArray,Math.atan((gamepad1.left_stick_y/gamepad1.left_stick_x))*180/Math.PI, (double) gamepad1.right_stick_x);
     }
 }
