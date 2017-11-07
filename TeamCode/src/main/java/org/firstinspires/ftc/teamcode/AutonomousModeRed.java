4015acb57669d42ec8367b3ee2d0e1535e9a625c
Caz-person <cullimore.cazmire@vashonsd.org>
2017-11-06 18:55:02 -0800

    changed autos into one base inherited by classes for their respective sides added code to quit when lost pictograph removed positioning code (not doing anything)

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonomousModeRed.java
@@ -0,0 +1,20 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+
+
+
+
+/**
+ * Created by FTC on 9/23/2017.
+ * updated by Caz
+ */
+// made for red in corner
+@Autonomous(name = "Vashon 5961 AutonomousRed", group = "Vashon 5961")
+// made for red in the corner
+class AutonomousModeRed extends AutonomousModeBase {
+
+    public AutonomousModeRed() {
+        super(true);
+    }
+}
